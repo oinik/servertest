51d09cd2ff65663cc44fd3fd94f2709174a99bd1
Jolth <jolthgs@gmail.com>
2024-01-08 13:48:05 -0500

    add license

--- tcp-server-GPS103.js
+++ tcp-server-GPS103.js
@@ -1,6 +1,22 @@
+/*
+ * Copyright 2024 Jorge Toro Hoyos (jolthgs@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 'use strict';
 
-const net = require('node:net')
+const net = require('node:net');
 
 const HOSTNAME = '';
 const PORT = 10101;
@@ -13,9 +29,8 @@ net.createServer(socket => {
     //socket.write(data);
     socket.pipe(socket);
 
-    socket.on('data', (data) => {
-        //console.log(data.toString());
-        console.log(`${new Date()} "${data.toString()}"`);
+    socket.on('data', (chunk) => {
+        console.log(`${new Date()} [${chunk.length}] "${chunk.toString()}"`);
     });
 
     socket.on('end', () => {
@@ -27,5 +42,5 @@ net.createServer(socket => {
     });
 
 }).listen({port: PORT, host: HOSTNAME}, () => {
-    console.log('Server running...');
+    console.log(`${new Date()} :: server ready and listening on port ${PORT}`);
 });
