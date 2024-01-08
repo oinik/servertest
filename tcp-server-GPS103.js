8f3b09d15e8d7a86146c4f046e8cbbc38dbb71d8
Jolth <jolthgs@gmail.com>
2024-01-08 11:22:36 -0500

    add event listener: error and end

--- tcp-server-GPS103.js
+++ tcp-server-GPS103.js
@@ -3,20 +3,27 @@
 const net = require('node:net')
 
 const HOSTNAME = '';
-const PORT = 60101;
+const PORT = 10101;
 
 net.createServer(socket => {
     //socket.setEncoding('utf8');
-    console.log('Client connected');
+    console.log("Client connected", socket.address());
 
     // echo
-    //socket.write(data)
+    //socket.write(data);
     socket.pipe(socket);
 
     socket.on('data', (data) => {
-        console.log(data.toString());
-        //socket.write(data)
-        //socket.pipe(socket);
+        //console.log(data.toString());
+        console.log(`${new Date()} "${data.toString()}"`);
+    });
+
+    socket.on('end', () => {
+        console.log("Closing connection with the client: ", socket.address());
+    });
+
+    socket.on('error', (err) => {
+        console.log(`Error: ${err}`);
     });
 
 }).listen({port: PORT, host: HOSTNAME}, () => {
